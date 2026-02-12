997416757117d1137d843803e4bc4bf66128a4b9
aiizennn <ashray1427@gmail.com>
2026-02-13 04:30:36 +0530

    second phase done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import { Server } from "socket.io";
+
 dotenv.config({
     path:"./.env",
 });
@@ -19,8 +20,28 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname,'public')));
 
+const users = [];
+
 io.on('connection',(socket)=>{
-    console.log('A new user joinded -> ',socket.id)
+    console.log('User connected -> ',socket.id);
+    
+    socket.on("join",(username)=>{
+        users[socket.id] = username;
+        console.log(username,"joined the chat");
+    })
+    socket.on("user-message", (data) => {
+        
+        const userData = {
+            username: users[socket.id],
+            message:data.message,
+            id:socket.id
+        }
+        io.emit("receive-message",userData);
+    });
+    // socket.on('disconnect',()=>{
+    //     delete users[socket.id];
+    //     console.log("User disconnected ->",socket.id);
+    // })
 })
 
 server.listen(PORT,()=>{
--- public/script.js
+++ public/script.js
@@ -1,6 +1,49 @@
-const socket = io();
+document.addEventListener("DOMContentLoaded", () => {
+  const socket = io();
 
-const sendBtn = document.getElementById("sendBtn");
-const msgInput = document.getElementById("message");
+  const sendBtn = document.getElementById("sendBtn");
+  const msgInput = document.getElementById("message");
+  const allMessage = document.getElementById("messages");
 
-msgInput.onclick()
+  const username = prompt("Enter your username : ");
+  socket.emit("join", username);
+
+  // socket.on("connect", () => {
+  //   console.log(socket.id);
+  // });
+
+  // socket.on("disconnect", () => {
+  //   console.log(socket.id);
+  // });
+  sendBtn.addEventListener("click", () => {
+    const message = msgInput.value.trim();
+    socket.emit("user-message", { message });
+    msgInput.value = "";
+  });
+
+  //   socket.on("user-message", (message) => {
+  //     const p = document.createElement("p");
+  //     p.textContent = message;
+  //     allMessage.appendChild(p);
+  //   });
+
+  socket.on("receive-message", (data) => {
+    const div = document.createElement("div");
+    div.classList.add("message");
+
+    if (data.id === socket.id) {
+      div.classList.add("self");
+    } else {
+      div.classList.add("other");
+    }
+
+    div.innerText = `${data.username}: ${data.message}`;
+    allMessage.appendChild(div);
+
+    allMessage.scrollTop = allMessage.scrollHeight;
+  });
+
+  msgInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") sendBtn.click();
+  });
+});
